f20d462297743c5f351ccf42d4f5acccd0f304b1
harshwardhanpatil <harshwardhanpatilsgu@gmail.com>
2023-08-05 19:14:19 +0530

    Day 1

---
+++ Lessons/lesson1.js
@@ -0,0 +1,13 @@
+// 1.Hello World
+console.log("Hello world!")
+
+// // Variables 
+var firstname = "Harsh"
+let lastname = "Patil"
+console.log(firstname)
+console.log(lastname)
+
+//Constants
+const occupation = "Manager"
+occupation = "Lead"
+console.log(occupation)
